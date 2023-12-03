1cdc6ecb267d7612830df300f532ca9d78b771f6
Daniel Wolbach <77749247+danielwolbach@users.noreply.github.com>
2023-12-03 23:28:19 +0100

    Remove insufficient docs

--- src/retro/mod.rs
+++ src/retro/mod.rs
@@ -14,29 +14,6 @@ pub use data::AudioData;
 pub use data::ContentData;
 pub use data::VideoData;
 
-/// Retrieves the content data, if available.
-///
-/// This function attempts to acquire a lock on the global CONTENT_DATA state
-/// and returns an `Option<ContentData>` representing the content data if
-/// the lock is successfully acquired.
-///
-/// # Returns
-/// - `Some(ContentData)`: If the lock is successfully acquired, returns a clone
-///   of the content data.
-/// - `None`: If the lock cannot be acquired or if the content data is not available.
-///
-/// # Examples
-/// ```
-/// # use your_crate_name::get_content_data;
-/// // Attempt to retrieve content data.
-/// if let Some(content_data) = get_content_data() {
-///     // Process the content data.
-///     println!("Content Title: {}", content_data.title);
-/// } else {
-///     // Handle the case when content data is not available.
-///     println!("Content data is not available.");
-/// }
-/// ```
 pub fn get_content_data() -> Option<ContentData> {
     state::CONTENT_DATA.lock().ok()?.clone()
 }
